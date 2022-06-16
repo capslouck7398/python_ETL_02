7eb6fe0ccecf243d2d5e7accf38db7f42f014f07
will <will31415926535@gmail.com>
2022-06-16 13:46:44 +0800

    homework_02_06

--- 321.py
+++ 321.py
@@ -18,6 +18,6 @@ for i in url_array:
   timeString = time.strftime("%Y-%m-%d-%I:%M:%S-%P", struct_time)
   respone = json.loads(ur.urlopen("https://dataservice.accuweather.com/forecasts/v1/hourly/12hour/"+i+"?apikey="+apiurl2+"&language=zh-TW&details=false&metric=false").read().decode('utf-8'))
   doc_ref = db.collection("homework").document("student02").collection("test02")
-  doc_ref.document('Taiwan-W-'+i+"-"+timeString).set({'weather':respone})
+  doc_ref.document('Taiwan-W-'+timeString+"-"+i).set({'weather':respone})
 
 
